02529409da943e6cd26754b0f9f511bd037a6d86
jsm1p2 <vinod.k@jurysoft.com>
2024-05-28 15:55:34 +0530

    added all dynamic services

--- src/components/service-details/ServiceDetailsMain.tsx
+++ src/components/service-details/ServiceDetailsMain.tsx
@@ -6,10 +6,10 @@ import blogTwo from "../../../public/assets/img/blog/blog-2.jpg";
 import ServiceSidebar from './ServiceSidebar';
 import FaqContent from '@/sheardComponent/elements/FaqContent';
 const ServiceDetailsMain = ({ id }: idType) => {
-    const service:any = serviceDataTwo.find((item)=> item.title == id)
+    const service: any = serviceDataTwo.find((item) => item.title == id)
     return (
         <>
-             <section className="bd-service-details-area pt-5 pb-10">
+            <section className="bd-service-details-area pt-5 pb-10">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
@@ -18,10 +18,10 @@ const ServiceDetailsMain = ({ id }: idType) => {
                                     <Image src={service.img} style={{ width: "100%", height: "auto" }} alt="img not found" />
                                 </div> */}
                                 {/* <p className="mb-30">When I skim through the documentation of popular frameworks, I find the features described in Part 1 straight away. When I read the web platform documentation (for example, on MDN), I find many confusing patterns of how to do things, without a conclusive representation of data-binding, list synchronization, or reactivity. I will try to draw some guidelines of how to approach these problems on the web platform, without requiring a framework (in other words, by going vanilla).</p> */}
-                              
-                                {service.data.map((e: { desc: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; },i: React.Key | null | undefined)=>
-                                <p key={i} className="mb-30">{e.desc}</p>
-                                )}
+
+                                {service.data.map((e: { desc: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined }, i: number) => (
+                                    <p key={i} className="mb-8">{e.desc}</p>
+                                ))}
                                 {/* <div className="row pb-10">
                                     <div className="col-xl-5 col-lg-6">
                                         <div className="bd-service-details-feature mb-30">
@@ -52,7 +52,7 @@ const ServiceDetailsMain = ({ id }: idType) => {
                         </div>
                     </div>
                 </div>
-            </section> 
+            </section>
         </>
     );
 };
