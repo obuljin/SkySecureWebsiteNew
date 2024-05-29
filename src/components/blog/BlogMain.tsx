cb8ffdbd0954c100107b61cb18209378f5cb4546
V - Tejeswar Reddy Pothu <v-tejeswar@skysecuretech.com>
2024-05-29 14:33:17 +0530

    breadcrems

--- src/components/blog/BlogMain.tsx
+++ src/components/blog/BlogMain.tsx
@@ -2,11 +2,10 @@ import Breadcrumb from '@/sheardComponent/Breadcrumb';
 import React from 'react';
 import BlogSection from './BlogSection';
 
-
 const BlogMain = () => {
     return (
         <>
-            <Breadcrumb pageTitle='Blog - SkySecure' pageSmallTitle='Blog'/>
+            <Breadcrumb pageTitle='Blog - SkySecure' pageSmallTitle='Blog' backgroundImage='assets/img/blog/Blog.png'/>
             <BlogSection/>
         </>
     );
