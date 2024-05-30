fb682b7eb6774ca2a89911bbbd68ea17fc5a6589
jsm1p2 <vinod.k@jurysoft.com>
2024-05-30 11:57:51 +0530

    fixed alignment issues

--- src/utils/SocilaLinks.tsx
+++ src/utils/SocilaLinks.tsx
@@ -4,16 +4,16 @@ import React from "react";
 const SocilaLinks = () => {
   return (
     <>
-      <Link href="https://www.facebook.com">
-        <i className="fab fa-facebook-f"></i>
+      <Link href="https://www.linkedin.com/company/skysecure-technologies/">
+        <i className="fab fa-linkedin"></i>
       </Link>
-      <Link href="https://www.twitter.com">
+      <Link href="https://twitter.com/SkysecureT?t=eIQh6SBZr52JEPj57ntPWw&amp;s=09/">
         <i className="fab fa-twitter"></i>
       </Link>
-      <Link href="https://www.instagram.com">
+      <Link href="https://www.instagram.com/skysecuretech?igsh=ZmgxdjIzZ2JjcGhw">
         <i className="fab fa-instagram"></i>
       </Link>
-      <Link href="https://www.youtube.com">
+      <Link href="https://www.youtube.com/channel/UCdCps8R7SdXhW3YQnE6tsyg">
         <i className="fab fa-youtube"></i>
       </Link>
     </>
